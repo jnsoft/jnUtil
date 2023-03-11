9fe995450e43664136d63f231d39773c1ead9620
Johan Nilsson <jnsoft@users.noreply.github.com>
2023-03-11 17:42:01 +0100

    alphanumComparator

--- jnUtil/Usings.cs
+++ jnUtil/Usings.cs
@@ -4,3 +4,4 @@ global using System.Linq;
 global using System.Text;
 global using System.Threading.Tasks;
 global using System.Globalization;
+global using System.Collections;
---
+++ jnUtilTests/AlphaNumComparatorTests.cs
@@ -0,0 +1,19 @@
+﻿namespace jnUtilTests;
+
+[TestClass]
+public class AlphaNumComparatorTests
+{
+    [TestMethod]
+    public void AlphaNumSortTest()
+    {
+        // Arrange
+        List<string> ss = new string[] {"item20", "item19", "item1", "item10", "item2"}.ToList();
+        List<string> ss2 = new string[] { "item1", "item2", "item10", "item19", "item20" }.ToList();
+
+        // Act
+        ss.Sort(new AlphaNumComparator());
+
+        // Assert
+        CollectionAssert.AreEqual(ss2, ss);
+    }
+}
