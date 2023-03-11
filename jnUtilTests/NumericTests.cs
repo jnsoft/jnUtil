4c2e68b157597080a196c559319efeb2f92fa459
Johan Nilsson <jnsoft@users.noreply.github.com>
2023-03-11 16:13:27 +0100

    numeric helper done

---
+++ jnUtilTests/NumericTests.cs
@@ -0,0 +1,24 @@
+﻿namespace jnUtilTests
+{
+    [TestClass]
+    public class NumericTests
+    {
+        [TestMethod]
+        public void DoubleNearlyEqualTest()
+        {
+            // Arrange
+            double d1 = 0.1;
+            double d2 = 0.4;
+            double d3 = 0.3;
+
+            // Act
+            double diff = d2 - d3;
+            bool test = NumericHelper.IsNearlyEqual(d1, diff);
+
+
+            // Assert
+            Assert.AreNotEqual(d1, diff);
+            Assert.IsTrue(test, $"Assert failed: {d1} does not nearly equals {diff}");
+        }
+    }
+}
